0fd2c6fc8040e1aa9d1616b8d00a98a3711f9761
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-11-07 14:23:52 -0300

    feat: :bug: Debugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
 
+app.use(json());
+
 app.get('/', (request, response) => {
   response.send('Hello World');
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 });
 
 app.listen(3333, () => console.log('Server started on port 3333'));
