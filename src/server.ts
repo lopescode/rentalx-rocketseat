1dec78d5c0c7ba8c82fe50ea624893d648bb0860
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-11-07 15:34:05 -0300

    feat: :sparkles: Inserindo ID com UUID

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(json());
 
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => console.log('Server started on port 3333'));
