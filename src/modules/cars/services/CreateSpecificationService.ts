808ef8f63192df2bc0a49550aaeb12692ead1343
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-11-07 19:03:20 -0300

    feat: :sparkles: Criando repositório de especificação

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,21 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    return null;
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationsAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationsAlreadyExists) {
+      throw Error('Specification already exists');
+    }
+    this.specificationsRepository.create({ name, description });
   }
 }
 
